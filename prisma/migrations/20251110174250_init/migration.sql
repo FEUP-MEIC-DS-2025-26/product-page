f2274d228d0941a2cede9580a4f3838b3f9d6a5a
Francisco Borralho <up201806242@edu.fe.up.pt>
2025-11-10 17:43:58 +0000

    Fixed setup and env files

---
+++ prisma/migrations/20251110174250_init/migration.sql
@@ -0,0 +1,122 @@
+-- CreateEnum
+CREATE TYPE "VoteType" AS ENUM ('LIKE', 'DISLIKE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "photo_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "storytelling" TEXT,
+    "description" TEXT,
+    "price" DECIMAL(10,2) NOT NULL,
+    "avg_score" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "specifications" JSONB,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+    "created_by_user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductPhoto" (
+    "id" SERIAL NOT NULL,
+    "photo_url" TEXT NOT NULL,
+    "alt_text" TEXT,
+    "is_main" BOOLEAN NOT NULL DEFAULT false,
+    "product_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductPhoto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" SERIAL NOT NULL,
+    "score" INTEGER NOT NULL,
+    "comment" TEXT,
+    "like_count" INTEGER NOT NULL DEFAULT 0,
+    "dislike_count" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+    "product_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Wishlist" (
+    "user_id" INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Wishlist_pkey" PRIMARY KEY ("user_id","product_id")
+);
+
+-- CreateTable
+CREATE TABLE "ReviewVote" (
+    "user_id" INTEGER NOT NULL,
+    "review_id" INTEGER NOT NULL,
+    "vote_type" "VoteType" NOT NULL,
+
+    CONSTRAINT "ReviewVote_pkey" PRIMARY KEY ("user_id","review_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "ProductPhoto_product_id_idx" ON "ProductPhoto"("product_id");
+
+-- CreateIndex
+CREATE INDEX "Review_product_id_idx" ON "Review"("product_id");
+
+-- CreateIndex
+CREATE INDEX "Review_user_id_idx" ON "Review"("user_id");
+
+-- CreateIndex
+CREATE INDEX "Wishlist_product_id_idx" ON "Wishlist"("product_id");
+
+-- CreateIndex
+CREATE INDEX "ReviewVote_review_id_idx" ON "ReviewVote"("review_id");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_created_by_user_id_fkey" FOREIGN KEY ("created_by_user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductPhoto" ADD CONSTRAINT "ProductPhoto_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Wishlist" ADD CONSTRAINT "Wishlist_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Wishlist" ADD CONSTRAINT "Wishlist_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ReviewVote" ADD CONSTRAINT "ReviewVote_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ReviewVote" ADD CONSTRAINT "ReviewVote_review_id_fkey" FOREIGN KEY ("review_id") REFERENCES "Review"("id") ON DELETE CASCADE ON UPDATE CASCADE;
