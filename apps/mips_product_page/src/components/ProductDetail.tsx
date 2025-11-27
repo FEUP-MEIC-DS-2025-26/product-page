16464b1204af334025c7a9e497cb300b3ad9b210
Filipa Geraldes <filipag3raldes@gmail.com>
2025-11-27 08:05:13 +0000

    fix: force use of cloud api

--- apps/mips_product_page/src/components/ProductDetail.tsx
+++ apps/mips_product_page/src/components/ProductDetail.tsx
@@ -10,9 +10,7 @@ import { useTheme } from '@mui/material/styles';
 import { JumpsellerReview } from '../services/jumpsellerApi';
 
 // Define a URL base dependendo do ambiente
-const API_BASE_URL = typeof window !== 'undefined' && window.location.hostname === 'localhost'
-  ? 'http://localhost:3103/api' 
-  : 'https://api.madeinportugal.store/api/';
+const API_BASE_URL = 'https://api.madeinportugal.store/api/';
 
 // --- TYPES ---
 type ProductSpecification = {
